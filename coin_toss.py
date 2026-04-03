532eb593258feafffee9a344765c582f08898f90
detts5 <ddettman3@fingerlakes.edu>
2026-04-03 19:38:48 +0000

    comment

--- coin_toss.py
+++ coin_toss.py
@@ -1,12 +1,20 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
-########## ENTER YER CODE BELOW THIS LINE ##########
+# NAME:Dan Dettman 
+# DATE: 4/1/2026
+# BRIEF DESCRIPTION: This program simulates a coin flip.
+# A random number between 1 and 100 is generated.
+# If the number is 51 or greater, Tails is printed.
+# Otherwise, Heads is printed.
+ 
+
+import random
+
+number = random.randint(1, 100)
+
+if number >= 51:
+    print("Tails")
+else:
+    print("Heads")
 
 
 
@@ -17,51 +25,12 @@
 
 
 
-########### END YER CODE ABOVE THIS LINE ###########
+#It is critical in this class that you understand the concepts as we explore them because
+#those concepts are required understanding for entry level programming. Reliance on resources
+#like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
+#may impede your understanding. Please rate how well you understand the concepts in this lab: 
+#[ ] I understand very little about this lab.
+#[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
+#[X ] I pretty much get it.
+#[ ] I'm solid. Totally got it.
 
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-'''
-===== Coin Flipper =====
-Heads
-'''
-
-
-
-'''
-===== Coin Flipper =====
-Tails
-'''
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
-'''
-
-1. What was the hardest part of completing this lab? 
-
-
-
-
-
-
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
-'''
